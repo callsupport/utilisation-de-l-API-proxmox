7c52f518889dc5c1237378131647e80fea1a703c
Nassima Ayadi <gsmicrodev@gmail.com>
2023-11-30 13:21:38 +0100

    corrections

--- api.sh
+++ api.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
 # Variables
-PROXMOX_IP="<adresse_proxmox>"
-PROXMOX_USER="<nom_utilisateur_proxmox>"
-PROXMOX_PASSWORD="<mot_de_passe_proxmox>"
-NODE_NAME="<noeud_proxmox>"
+PROXMOX_IP="176.9.10.239"
+PROXMOX_USER="nassima-ayadi"
+PROXMOX_PASSWORD="Pleading-Gallon6-Hatred-Stagnant-Imperfect-Surger"
+NODE_NAME="wcs-cyber-node01"
+
+# Droit d'exécution
+chmod +x api.sh
 
 # Installation de jq si nécessaire
 install_jq() {
@@ -14,6 +17,8 @@ install_jq() {
             sudo apt-get install -y jq
         elif command -v yum &> /dev/null; then
             sudo yum install -y jq
+        elif command -v brew &> /dev/null; then
+            sudo brew install -y jq
         else
             echo "Erreur: Aucun gestionnaire de paquets pris en charge trouvé. Veuillez installer jq manuellement."
             exit 1
@@ -22,8 +27,12 @@ install_jq() {
 }
 
 # Récupération du Token 
+
+curl --silent --insecure --data "username=${PROXMOX_USER}@pam&password=${PROXMOX_PASSWORD}" https://${PROXMOX_IP}:8006/api2/json/access/ticket | jq --raw-output '.data.ticket' | sed 's/^/PVEAuthCookie=/' > cookie
+
+# Variable Token
 TOKEN=$(curl --silent --insecure --data "username=${PROXMOX_USER}@pam&password=${PROXMOX_PASSWORD}" \
- https://${PROXMOX_IP}:8006/api2/json/access/ticket | jq --raw-output '.data.ticket')
+ https://${PROXMOX_IP}:8006/api2/json/access/ticket \) | jq --raw-output '.data.ticket')
 
 # Vérification 
 if [ -z "$TOKEN" ]; then
@@ -32,8 +41,13 @@ if [ -z "$TOKEN" ]; then
 fi
 
 # Réception du CSRF Token
-CSRF_TOKEN=$(curl --silent --insecure --cookie "PVEAuthCookie=${TOKEN}" \
- https://${PROXMOX_IP}:8006/api2/json/access/ticket | jq --raw-output '.data.CSRFPreventionToken')
+curl --silent --insecure --data "username=${PROXMOX_USER}@pam&password=${PROXMOX_PASSWORD}" \
+ https://${PROXMOX_IP}:8006/api2/json/access/ticket \\n| jq --raw-output '.data.CSRFPreventionToken' | sed 's/^/CSRFPreventionToken:/' > csrftoken
+
+# Variable CSRF Token
+CSRF_TOKEN=$(curl --silent --insecure --data "username=${PROXMOX_USER}@pam&password=${PROXMOX_PASSWORD}" \
+ https://${PROXMOX_IP}:8006/api2/json/access/ticket \
+| jq --raw-output '.data.CSRFPreventionToken')
 
 # Vérification 
 if [ -z "$CSRF_TOKEN" ]; then
@@ -42,5 +56,6 @@ if [ -z "$CSRF_TOKEN" ]; then
 fi
 
 # Lister les machines virtuelles
-curl --insecure --cookie "PVEAuthCookie=${TOKEN}" --header "CSRFPreventionToken: ${CSRF_TOKEN}" \
- https://${PROXMOX_IP}:8006/api2/json/nodes/${NODE_NAME}/qemu
+
+curl  --insecure --cookie "$(<cookie)" https://${PROXMOX_IP}:8006/api2/json/nodes/${NODE_NAME}/qemu
+
